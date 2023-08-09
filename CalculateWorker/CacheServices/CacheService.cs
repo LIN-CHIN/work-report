9c43c197dc4ed9f67f031e086bb7692de6d6924b
LIN-CHIN <s235068267@gmail.com>
2023-08-09 15:26:46 +0800

    feat: add cache services

---
+++ CalculateWorker/CacheServices/CacheService.cs
@@ -0,0 +1,83 @@
+﻿using Newtonsoft.Json;
+using StackExchange.Redis;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculateWorker.CacheServices
+{
+    /// <summary>
+    /// Cache Service
+    /// </summary>
+    public class CacheService : ICacheService
+    {
+        private IDatabase _db;
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        public CacheService()
+        {
+            ConfigureRedis();
+        }
+        
+        /// <summary>
+        /// 設定Redis
+        /// </summary>
+        private void ConfigureRedis()
+        {
+            string redisHost = "redis";
+            int redisPort = 6379; 
+
+            string connectionString = $"{redisHost}:{redisPort},allowAdmin=true,abortConnect=false";
+            ConnectionMultiplexer redis = ConnectionMultiplexer.Connect(connectionString);
+            _db = redis.GetDatabase(0);
+
+            Console.WriteLine("連結成功");
+        }
+
+        ///<inheritdoc/>
+        public T GetData<T>(string key)
+        {
+            var value = _db.StringGet(key);
+            if (!string.IsNullOrEmpty(value))
+            {
+                return JsonConvert.DeserializeObject<T>(value);
+            }
+            return default;
+        }
+
+        ///<inheritdoc/>
+        public bool SetData<T>(string key, T value, DateTimeOffset expirationTime)
+        {
+            TimeSpan expiryTime = expirationTime.DateTime.Subtract(DateTime.Now);
+            var isSet = _db.StringSet(key, JsonConvert.SerializeObject(value), expiryTime);
+            return isSet;
+        }
+
+        ///<inheritdoc/>
+        public object RemoveData(string key)
+        {
+            bool _isKeyExist = _db.KeyExists(key);
+            if (_isKeyExist == true)
+            {
+                return _db.KeyDelete(key);
+            }
+            return false;
+        }
+
+        ///<inheritdoc/>
+        public bool IsKeyExist(string key) 
+        {
+            return _db.KeyExists(key);
+        }
+
+        ///<inheritdoc/>
+        public long IncreaseKey(string key, long value = 1)
+        {
+           return _db.StringIncrement(key, value);
+        }
+    }
+}
---
+++ CalculateWorker/CacheServices/ICacheService.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculateWorker.CacheServices
+{
+    /// <summary>
+    /// Cache Service Interface
+    /// </summary>
+    public interface ICacheService
+    {
+        /// <summary>
+        /// 根據key取得資料
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="key"></param>
+        /// <returns></returns>
+        T GetData<T>(string key);
+
+        /// <summary>
+        /// 設定Data
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="key"></param>
+        /// <param name="value"></param>
+        /// <param name="expirationTime"></param>
+        /// <returns></returns>
+        bool SetData<T>(string key, T value, DateTimeOffset expirationTime);
+
+        /// <summary>
+        /// 刪除Data
+        /// </summary>
+        /// <param name="key"></param>
+        /// <returns></returns>
+        object RemoveData(string key);
+
+        /// <summary>
+        /// 檢查key是否存在
+        /// </summary>
+        /// <param name="key"></param>
+        /// <returns></returns>
+        bool IsKeyExist(string key);
+
+        /// <summary>
+        /// 遞增
+        /// </summary>
+        /// <param name="key"></param>
+        /// <param name="value"></param>
+        /// <returns></returns>
+        long IncreaseKey(string key, long value = 1);
+    }
+}
